fe066273fd34373360913c70dfe6179892e22704
deanefraim1 <deanefraim@campus.technion.ac.il>
2022-12-16 17:26:19 +0200

    .

--- Bank.cpp
+++ Bank.cpp
@@ -3,23 +3,40 @@
 #include "ATM.hpp"
 #include <string>
 #include <unistd.h>
+#include <iostream>
+
+extern LogManager *logManager;
+
+using namespace std;
 
 Bank::Bank()
 {
-    pthread_create(&thread, NULL, Bank::RunBank, this);
+    pthread_create(&commissionThread, NULL, Bank::RunBankCommision, this);
+    pthread_create(&statusThread, NULL, Bank::RunBankStatus, this);
 }
 
 Bank::~Bank()
 {
-    pthread_exit(NULL);
+    pthread_exit(&commissionThread);
+    pthread_exit(&statusThread);
 }
 
-void *Bank::RunBank(void *bankToRunAsVoid)
+void *Bank::RunBankCommision(void *bankToRunAsVoid)
 {
     Bank *bankToRun = (Bank *) bankToRunAsVoid;
     while(sleep(3))
     {
         bankToRun->TakeCommissions();
+    }
+    return NULL;
+}
+
+void *Bank::RunBankStatus(void *bankToRunAsVoid)
+{
+    Bank *bankToRun = (Bank *) bankToRunAsVoid;
+    while(usleep(500000))
+    {
+        bankToRun->PrintStatus();
     }
     return NULL;
 }
@@ -35,6 +52,20 @@ void Bank::TakeCommissions()
         amoutToTake = accounts[currentAccount].balance * commissionInPrecentage;
         accounts[currentAccount].balance -= amoutToTake;
         this->balance += amoutToTake;
+        logManager->PrintToLog("Bank: commissions of " + to_string(commissionInPrecentage) + "% were charged, the bank gained " + to_string(amoutToTake) + "$ from account " + to_string(accounts[currentAccount].id));
     }
     ExitWriter();
+}
+
+void Bank::PrintStatus()
+{
+    EnterReader();
+    printf("\033[2J"); //clear screen
+    printf("\033[1;1H");//moves cursor to beginning of screen
+    cout << "Current Bank Status" << endl;
+    for (size_t currentAccount = 0; currentAccount < accounts.size(); currentAccount++)
+    {
+        cout << "Account " + to_string(accounts[currentAccount].id) + ": Balance – " + to_string(accounts[currentAccount].balance) + "$, Account Password – " + to_string(accounts[currentAccount].password) << endl;
+    }
+    ExitReader();
 }
